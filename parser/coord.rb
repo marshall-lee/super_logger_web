9d899f847a86c617a8a65ab4a1ed14126d4a9e60
Vladimir Kochnev <hashtable@yandex.ru>
2015-02-22 03:09:57 +0300

    improve dynmap integration

--- parser/coord.rb
+++ parser/coord.rb
@@ -2,4 +2,8 @@ class Coord < Struct.new(:world_name, :x, :y, :z)
   def href
     "http://mc.lemonspace.me/?worldname=#{world_name}&mapname=surface&x=#{x}&y=#{y}&z=#{z}&zoom=6"
   end
+
+  def to_s
+    "(#{x},#{y},#{z})"
+  end
 end
--- parser/player.rb
+++ parser/player.rb
@@ -3,6 +3,10 @@ class Player < Struct.new(:nick, :uid)
     "##{'%06X' % (uid.hash & 0xFFFFFF)}"
   end
 
+  def href
+    "http://mc.lemonspace.me/?playername=#{nick}&mapname=surface&zoom=6"
+  end
+
   def to_s
     "<#{nick}>"
   end
