a0d3e467313bc56b8c23d471a39c2cb4099815b0
Vladimir Kochnev <hashtable@yandex.ru>
2015-02-23 22:03:54 +0300

    use crc32 instead of #hash to avoid color changes after restarts

--- parser/player.rb
+++ parser/player.rb
@@ -1,13 +1,20 @@
+require 'zlib'
+
 class Player < Struct.new(:entry, :nick, :uid)
   def color
-    "##{'%06X' % (uid.hash & 0xFFFFFF)}"
+    "##{'%06X' % (colorizable_hash & 0xFFFFFF)}"
   end
 
   def color_cls
-    "nick color%d" % (uid.hash & 7)
+    "nick color%d" % (colorizable_hash & 7)
   end
 
   def href
     "http://mc.lemonspace.me/?playername=#{nick}&mapname=#{entry.coord.map_name}&zoom=6"
   end
+
+  private
+    def colorizable_hash
+      Zlib::crc32(uid || nick)
+    end
 end
